2538cd23e2aa7c10612d3e124bd68a3ed43d62b4
Piblidu17 <56850477+Piblidu17@users.noreply.github.com>
2019-12-07 16:19:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
-    int i, j, n1, n2;
+    int i, n1, n2, ac = 0;
     int *cad1, *cad2;
     int *result;
     n1 = 100;
@@ -13,13 +13,14 @@ int main() {
     scanf("%d",cad2);
     result = (int*)malloc(n2*sizeof(int));
     for (i = n1; i >= 0; i--){
-        *(result + i) = (*(cad1 + i) + *(cad2 + i));
+        *(result + i) = (*(cad1 + i) + *(cad2 + i) + ac);
         if (*(result + i) >= 10){
             *(result + i) = (*(cad1 + i) + *(cad2 + i)) % 10;
-        }
-        else {
-            *(result + i) = *(cad1 + i) + *(cad2 + i);
+            ac = 1;
         }
         printf("%d",*(result+i));
-    } 
+    }
+    free(cad1);
+    free(cad2);
+    free(result);
 }
