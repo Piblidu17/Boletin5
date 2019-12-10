61d361f222df14cafdfa93239f57b453ebd885cf
Piblidu17 <56850477+Piblidu17@users.noreply.github.com>
2019-12-10 18:13:31 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main() {
-    int i, n1, n2, ac = 0;
-    int *cad1, *cad2;
-    int *result;
-    n1 = 100;
-    n2 = 100;
-    cad1 = (int*)malloc(n1* sizeof(int));
-    cad2 = (int*)malloc(n2* sizeof(int));
-    scanf("%d",cad1);
-    scanf("%d",cad2);
-    result = (int*)malloc(n2*sizeof(int));
-    for (i = n1; i >= 0; i--){
-        *(result + i) = (*(cad1 + i) + *(cad2 + i) + ac);
-        if (*(result + i) >= 10){
-            *(result + i) = (*(cad1 + i) + *(cad2 + i)) % 10;
-            ac = 1;
-        }
-        printf("%d",*(result+i));
+bignum str2bignum(char *str) {        //converte unha cadea a bignum.
+    bignum bn;
+    int i,j;
+    j = 0;
+    if(str[0] == '-'){
+        bn.sign = -1;
     }
-    free(cad1);
-    free(cad2);
-    free(result);
+    else if (str[0] == '+'){
+        bn.sign = 1;
+    }
+    bn.tam = strlen(str)-1;
+    bn.val = (int*)malloc(bn.tam*(sizeof(int)));
+    for(i = 1; i <= bn.tam; i++){
+        *(bn.val + j) = *(str + i) % 48;
+        j++;
+    }
+    return bn;
 }
-
-
-
 
 
 #include <stdio.h>
